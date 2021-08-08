5d1e755f9de6568af7fa965a3b6928026bab8871
MacSergey <macsergey@me.com>
2021-08-09 00:07:54 +0300

    Update version

--- NetworkMultitool/Properties/AssemblyInfo.cs
+++ NetworkMultitool/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.1.0.257")]
 [assembly: AssemblyFileVersion("1.1.0.257")]
 #else
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.1.0.2")]
+[assembly: AssemblyFileVersion("1.1.0.2")]
 #endif
 
