6965db70f3a487286ca80259248834063a684890
MacSergey <macsergey@me.com>
2021-08-25 01:10:31 +0300

    Update common

--- NetworkMultitool/Properties/AssemblyInfo.cs
+++ NetworkMultitool/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("741e3213-4964-4600-a82c-7ffafef72449")]
 
 #if BETA
-[assembly: AssemblyVersion("1.3.0.7")]
-[assembly: AssemblyFileVersion("1.3.0.7")]
+[assembly: AssemblyVersion("1.3.0.8")]
+[assembly: AssemblyFileVersion("1.3.0.8")]
 #else
 [assembly: AssemblyVersion("1.2.0.1")]
 [assembly: AssemblyFileVersion("1.2.0.1")]
