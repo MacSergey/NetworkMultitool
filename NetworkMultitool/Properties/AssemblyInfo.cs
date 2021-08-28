389dd1381f724c1ad625c203a7d2267b8682cae3
MacSergey <macsergey@me.com>
2021-08-28 23:45:18 +0300

    Added Dutch translation

--- NetworkMultitool/Properties/AssemblyInfo.cs
+++ NetworkMultitool/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("741e3213-4964-4600-a82c-7ffafef72449")]
 
 #if BETA
-[assembly: AssemblyVersion("1.3.0.29")]
-[assembly: AssemblyFileVersion("1.3.0.29")]
+[assembly: AssemblyVersion("1.3.0.30")]
+[assembly: AssemblyFileVersion("1.3.0.30")]
 #else
 [assembly: AssemblyVersion("1.2.0.2")]
 [assembly: AssemblyFileVersion("1.2.0.2")]
